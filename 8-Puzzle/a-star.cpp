efe3f74348ae20da7d05717456e717e50c56c923
unknown <saskinner19@gmail.com>
2023-02-18 00:15:40 -0500

    added early goal check in bfs.cpp & added a*

--- 8-Puzzle/a-star.cpp
+++ 8-Puzzle/a-star.cpp
@@ -18,7 +18,7 @@ vector<vector<int>> GOAL_STATE = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}};
 //      {1,3,0},
 //      {6,4,7}
 //  };
-//  vector<vector<int>> INITIAL_STATE = {
+// vector<vector<int>> INITIAL_STATE = {
 //      {2,5,8},
 //      {1,3,7},
 //      {6,4,0}
@@ -35,6 +35,7 @@ vector<vector<int>> GOAL_STATE = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}};
 //     {1,3,7},
 //     {0,6,4}
 // };
+
 // 15 steps
 vector<vector<int>> INITIAL_STATE = {
     {2, 5, 8},
@@ -45,7 +46,14 @@ vector<vector<int>> INITIAL_STATE = {
 // vector<vector<int>> INITIAL_STATE = {
 //     {8, 1, 2},
 //     {7, 0, 3},
-//     {6, 5, 4}};
+//     {6, 5, 4}
+// };
+
+// vector<vector<int>> INITIAL_STATE = {
+//     {7, 2, 4},
+//     {5, 0, 6},
+//     {8, 3, 1}
+// };
 
 struct Node
 {
@@ -69,9 +77,9 @@ void printState(vector<vector<int>> state)
     {
         for (int j = 0; j < state[i].size(); j++)
         {
-            cout << state[i][j] << " ";
+            std::cout << state[i][j] << " ";
         }
-        cout << endl;
+        std::cout << endl;
     }
 }
 
@@ -205,87 +213,119 @@ vector<Node> expand(Node node)
     return children;
 }
 
-Node bfs(vector<vector<int>> initial_state, int *frontier_size, int *reached_size)
+int manhattanDistance(vector<vector<int>> state)
 {
-    // Initialize the starting node
-    Node root(initial_state, NULL, "", 0);
-
-    // Check if the initial state is the goal state
-    if (isGoalState(root.state))
+    // Calculates the manhattan distance of the current state
+    int distance = 0;
+    // For each tile in the state calculate the manhattan distance
+    for (int i = 0; i < state.size(); i++)
     {
-        *frontier_size = 0;
-        *reached_size = 1;
-        return root;
+        for (int j = 0; j < state[i].size(); j++)
+        {
+            // If the tile is the blank tile, skip it
+            if(state[i][j] == 0)
+                continue;
+            // tile is the value at the current row and column
+            int tile = state[i][j];
+            // the goal row and column is where the tile should be
+            int goal_row = tile / 3;
+            int goal_col = tile % 3;
+            // calculate the distance between the two points.
+            distance += abs(i - goal_row) + abs(j - goal_col);
+        }
     }
+    return distance;
+}
 
-    // Initialize the frontier and reached
+int evaluate(Node node)
+{
+    // Calculates the evaluation function of the current node
+    return node.path_cost + manhattanDistance(node.state);
+}
+
+void sortFrontier(vector<Node> &frontier)
+{
+    for (int i = 0; i < frontier.size(); i++)
+    {
+        for (int j = 0; j < frontier.size() - 1; j++)
+        {
+            if (evaluate(frontier[j]) > evaluate(frontier[j + 1]))
+            {
+                Node temp = frontier[j];
+                frontier[j] = frontier[j + 1];
+                frontier[j + 1] = temp;
+            }
+        }
+    }
+}
+
+Node a_star(vector<vector<int>> initial_state, int *frontier_size, int *reached_size)
+{
+    vector<vector<int>> EMPTY = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+
+    // Create the initial node
+    Node initial = Node(initial_state, NULL, "", 0);
+
+    // Create the frontier and reached sets
     vector<Node> frontier;
-    frontier.push_back(root);
     vector<Node> reached;
 
-    // Loop until the frontier is empty
+    // Add the initial node to the frontier
+    frontier.push_back(initial);
+    reached.push_back(initial);
+
+    
+
+    // While the frontier is not empty
     while (frontier.size() != 0)
     {
-        // Pop the first element in the frontier
-        // Acts as a queue
+        sortFrontier(frontier);
+        // Get the node with the lowest evaluation function
         Node node = frontier[0];
         frontier.erase(frontier.begin());
+        
 
-        // Store element in reached.
-        reached.push_back(node);
-
-        // Check if the current nodes state is the goal state
+        // If the node is the goal state, return the node
         if (isGoalState(node.state))
         {
-            // If it is the goal state, return the node and the sizes of the frontier and reached
-            *reached_size = reached.size();
             *frontier_size = frontier.size();
+            *reached_size = reached.size();
             return node;
         }
 
-        // Expand the current node to get its children
+        // Expand the node
         vector<Node> children = expand(node);
 
-        // Loop over all current children
+        // For each child node
         for (int i = 0; i < children.size(); i++)
         {
-            // Flag determines if the child already is in reached
-            bool flag = false;
-
-            // Account for if one of the children attempted to move the blank tile out of bounds
-            if (children[i].state == vector<vector<int>>{{0, 0, 0}, {0, 0, 0}, {0, 0, 0}})
+            if (children[i].state == EMPTY)
                 continue;
-
-            if (isGoalState(node.state))
-            {
-                // If it is the goal state, return the node and the sizes of the frontier and reached
-                *reached_size = reached.size();
-                *frontier_size = frontier.size();
-                return node;
-            }
-
-            // Loop over all elements in reached to determine if child is already in reached
+            
+            bool flag = false;
+            vector<vector<int>> child_state = children[i].state;
+            int child_path_cost = children[i].path_cost;
+            int reached_path_cost = 0;
             for (int j = 0; j < reached.size(); j++)
             {
-                if (children[i].state == reached[j].state)
+                vector<vector<int>> reached_state = reached[j].state;
+
+                if (child_state == reached_state)
                 {
+                    reached_path_cost = reached[j].path_cost;
                     flag = true;
                     break;
                 }
             }
-
-            // If not in reached, add to reached.
-            // Also add to frontier to expand later
-            if (!flag)
-            {
-                frontier.push_back(children[i]);
-                reached.push_back(children[i]);
-            }
+            if (!flag || child_path_cost < reached_path_cost)
+                {
+                    frontier.push_back(children[i]);
+                    reached.push_back(children[i]);
+                }
         }
     }
 
-    // If the frontier is empty and no solution has been found, return the initial node.
-    return root;
+    return initial;
 }
 
 int main()
@@ -295,32 +335,32 @@ int main()
     // If no empty space then there is not solution
     if (findIndex(INITIAL_STATE)[0][0] == -1)
     {
-        cout << "Invalid initial state" << endl;
+        std::cout << "Invalid initial state" << endl;
         return 0;
     }
 
     // Print the initial state
     printState(INITIAL_STATE);
-    cout << endl;
+    std::cout << endl;
 
     // Time how long it takes for the algorithm to complete
     auto start = high_resolution_clock::now();
-    Node end = bfs(INITIAL_STATE, &frontier_size, &reached_size);
+    Node end = a_star(INITIAL_STATE, &frontier_size, &reached_size);
     auto stop = high_resolution_clock::now();
 
     // If the initial state is the goal state, no solution was found
     if (end.state == INITIAL_STATE)
     {
-        cout << "No solution found" << endl;
+        std::cout << "No solution found" << endl;
         return 0;
     }
 
     // If solution was found, print the solution and other information
     printState(end.state);
-    cout << "\nNumber of steps: " << end.path_cost << endl;
-    cout << "frontier size: " << frontier_size << endl;
-    cout << "reached size: " << reached_size << endl;
-    cout << "Time taken: " << duration_cast<milliseconds>(stop - start).count() << " milliseconds" << endl;
+    std::cout << "\nNumber of steps: " << end.path_cost << endl;
+    std::cout << "frontier size: " << frontier_size << endl;
+    std::cout << "reached size: " << reached_size << endl;
+    std::cout << "Time taken: " << duration_cast<milliseconds>(stop - start).count() << " milliseconds" << endl;
 
     return 0;
 }
